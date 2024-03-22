c14ed0b6cc08132652ee8f83084bd2084c79cc3b
Sarmad Malik <sarmadmalik336@gmail.com>
2024-03-23 01:19:23 +0530

    nds front

--- loginreg/admin.py
+++ loginreg/admin.py
@@ -1,1 +1,5 @@
 from django.contrib import admin
+from .models import User,Admin
+
+admin.site.register(User)
+admin.site.register(Admin)
--- loginreg/forms.py
+++ loginreg/forms.py
@@ -18,3 +18,7 @@ class RegisterForm(forms.ModelForm):
             'phone': 'Phone no.',
             'password': 'Password',
         }
+
+class LoginForm(forms.Form):
+    email = forms.EmailField(label='Email', widget=forms.TextInput(attrs={'class': 'form-control'}))
+    password = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
